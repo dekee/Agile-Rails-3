59e4957f3524f09c8c229641c94edcbc3f8aec78
Derrick Johnson <derrick@Derrick-Johnsons-MacBook-Pro.local>
2011-04-05 20:46:58 -0400

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AgileWebDevelopment::Application.config.secret_token = '36a9e6dec0cc6336c91e52b5d118c750ca389bcb923d94e47d6cf863be5846ef024c2363cb25bdcc17c3d3be26c823a8d4705e8a59e93b890c88df3160fd3364'
